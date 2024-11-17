67d369e60c97d94e53b87ceabef8aee175eab5ac
Igor Belousov <imbelousov66@gmail.com>
2024-11-17 22:49:41 +0500

    Basic functions

--- src/findany.c
+++ src/findany.c
@@ -1,9 +1,14 @@
 #include <getopt.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define PROGRAM_NAME "findany"
+
+int case_insensitive = 0;
+char* substrings_filename;
+char* input_filename = NULL;
 
 const struct option long_options[] = {
     {"case-insensitive", no_argument, NULL, 'i'},
@@ -30,34 +35,34 @@ void print_help()
     printf("  -h, --help                print this help\n");
 }
 
-#define READ_LINE_BUFFER_SIZE 4
+#define READ_LINE_BUFFER_SIZE 120
 #define READ_LINE_BUFFER_MARK 0x7F
 
-/*
+/**
  * Read the next line from the file
  * @param &buffer Address of the first character of the buffer or NULL.
  * If NULL, allocate memory automatically. Can expand if the next
- * line is longer than bufferSize-1.
- * @param &bufferSize Address of the variable containing size of the buffer
+ * line is longer than buffer_size-1.
+ * @param &buffer_size Address of the variable containing size of the buffer
  * @param fp Input file handle
  * @return Length of the line including \\n, or 0 if EOF
  */
-ssize_t read_line(char** buffer, size_t* bufferSize, FILE* fp)
+size_t read_line(char** buffer, size_t* buffer_size, FILE* fp)
 {
     char* buffer_ = *buffer;
-    size_t bufferSize_ = *bufferSize;
+    size_t buffer_size_ = *buffer_size;
 
     if (buffer_ == NULL)
     {
-        bufferSize_ = READ_LINE_BUFFER_SIZE;
-        buffer_ = malloc(bufferSize_);
+        buffer_size_ = READ_LINE_BUFFER_SIZE;
+        buffer_ = malloc(buffer_size_);
     }
-    buffer_[bufferSize_ - 1] = READ_LINE_BUFFER_MARK;
+    buffer_[buffer_size_ - 1] = READ_LINE_BUFFER_MARK;
 
     size_t offset = 0;
-    while (1)
+    while (true)
     {
-        int eof = fgets(buffer_ + offset, bufferSize_ - offset, fp) == NULL;
+        bool eof = fgets(buffer_ + offset, buffer_size_ - offset, fp) == NULL;
         if (eof)
         {
             if (!feof(fp))
@@ -67,33 +72,115 @@ ssize_t read_line(char** buffer, size_t* bufferSize, FILE* fp)
         }
 
         // Optimization which allows to skip full scan of the buffer in order to determine length of the line
-        if (buffer_[bufferSize_ - 1] == READ_LINE_BUFFER_MARK || buffer_[bufferSize_ - 2] == '\n')
+        if (buffer_[buffer_size_ - 1] == READ_LINE_BUFFER_MARK || buffer_[buffer_size_ - 2] == '\n')
         {
             offset += strlen(buffer_ + offset);
             break;
         }
 
-        offset = bufferSize_ - 1;
-        bufferSize_ *= 2;
-        buffer_ = realloc(*buffer, bufferSize_);
+        offset = buffer_size_ - 1;
+        buffer_size_ *= 2;
+        buffer_ = realloc(*buffer, buffer_size_);
         if (buffer_ == NULL)
             exit(EXIT_FAILURE);
-        buffer_[bufferSize_ - 1] = READ_LINE_BUFFER_MARK;
+        buffer_[buffer_size_ - 1] = READ_LINE_BUFFER_MARK;
     }
 
     *buffer = buffer_;
-    *bufferSize = bufferSize_;
+    *buffer_size = buffer_size_;
     return offset;
 }
 
-int case_insensitive = 0;
-char* substrings_filename;
-char* output_filename = NULL;
+#define TRIE_DEFAULT_SIZE 64 * 1024
+#define TRIE_NODE_SIZE sizeof(struct trie_node)
+#define TRIE_EMPTY_NODE {-1, -1, '\0', false}
 
-void build_trie()
+struct trie_node
 {
-    printf("Read file %s\n", substrings_filename);
+    /**
+     * Index of the next character in list
+     */
+    ssize_t idx_next;
 
+    /**
+    * Index of the first child node
+    */
+    ssize_t idx_child;
+
+    /**
+     * Stored character or \\0, if node is empty
+     */
+    char c;
+
+    /**
+     * Determines whether trie contains the word formed by this and parent nodes or not
+     */
+    bool is_leaf;
+};
+
+struct trie_node* trie;
+size_t trie_size;
+size_t trie_offset;
+
+void trie_init()
+{
+    trie_size = TRIE_DEFAULT_SIZE;
+    trie = malloc(trie_size * TRIE_NODE_SIZE);
+    struct trie_node root = TRIE_EMPTY_NODE;
+    trie[0] = root;
+    trie_offset = 1;
+}
+
+void trie_expand()
+{
+    trie_size *= 2;
+    trie = realloc(trie, trie_size * TRIE_NODE_SIZE);
+    if (trie == NULL)
+        exit(EXIT_FAILURE);
+}
+
+size_t trie_new_node()
+{
+    if (trie_size <= trie_offset)
+        trie_expand();
+    struct trie_node new_node = TRIE_EMPTY_NODE;
+    trie[trie_offset] = new_node;
+    return trie_offset++;
+}
+
+void trie_add(ssize_t idx, char* str, size_t length)
+{
+    char c = *str;
+    ssize_t idx_prev;
+    do
+    {
+        if (trie[idx].c == '\0' || trie[idx].c == c)
+        {
+            trie[idx].c = c;
+            break;
+        }
+        idx_prev = idx;
+        idx = trie[idx].idx_next;
+    }
+    while (idx >= 0);
+    if (idx < 0)
+    {
+        idx = trie_new_node();
+        trie[idx_prev].idx_next = idx;
+        trie[idx].c = c;
+    }
+    if (length <= 1)
+    {
+        trie[idx].is_leaf = true;
+        return;
+    }
+    if (trie[idx].idx_child < 0)
+        trie[idx].idx_child = trie_new_node();
+    trie_add(trie[idx].idx_child, str + 1, length - 1);
+}
+
+void trie_build()
+{
     FILE* fp = fopen(substrings_filename, "r");
     if (fp == NULL)
     {
@@ -101,15 +188,83 @@ void build_trie()
         exit(EXIT_FAILURE);
     }
 
-    char* buffer = NULL;
-    size_t bufferSize;
-    ssize_t read;
-    while ((read = read_line(&buffer, &bufferSize, fp)) > 0)
-    {
+    trie_init();
 
+    char* buffer = NULL;
+    size_t buffer_size;
+    size_t read;
+    while ((read = read_line(&buffer, &buffer_size, fp)) > 0)
+    {
+        if (buffer[read - 1] == '\n')
+            buffer[--read] = '\0';
+        if (read > 0 && buffer[read - 1] == '\r')
+            buffer[--read] = '\0';
+        trie_add(0, buffer, read);
     }
 
     fclose(fp);
+    free(buffer);
+}
+
+bool trie_find(ssize_t idx, char* str, size_t length)
+{
+    char c = *str;
+    do
+    {
+        if (trie[idx].c == c)
+            break;
+        idx = trie[idx].idx_next;
+    }
+    while (idx >= 0);
+    if (idx < 0)
+        return false;
+    if (trie[idx].is_leaf)
+        return true;
+    if (length <= 1)
+        return false;
+    return trie_find(trie[idx].idx_child, str + 1, length - 1);
+}
+
+bool filter_input(char* str, size_t length)
+{
+    for (size_t i = 0; i < length; i++)
+    {
+        if (trie_find(0, str + i, length - i))
+            return true;
+    }
+    return false;
+}
+
+void scan_input()
+{
+    FILE* fp = stdin;
+    bool need_close = false;
+    if (input_filename != NULL)
+    {
+        fp = fopen(input_filename, "r");
+        if (fp == NULL)
+        {
+            printf("No access to file %s", input_filename);
+            exit(EXIT_FAILURE);
+        }
+        need_close = true;
+    }
+
+    char* buffer = NULL;
+    size_t buffer_size;
+    size_t read;
+    while ((read = read_line(&buffer, &buffer_size, fp)) > 0)
+    {
+        if (buffer[read - 1] == '\n')
+            read--;
+        if (read > 0 && buffer[read - 1] == '\r')
+            read--;
+        if (filter_input(buffer, read))
+            fwrite(buffer, 1, read, stdout);
+    }
+
+    if (need_close)
+        fclose(fp);
     free(buffer);
 }
 
@@ -146,11 +301,10 @@ int main(int argc, char **argv)
 
         switch (argc - opti)
         {
+        case 2:
+            input_filename = argv[opti + 1];
         case 1:
             substrings_filename = argv[opti];
-
-        case 2:
-            output_filename = argv[opti + 1];
             break;
 
         default:
@@ -159,7 +313,7 @@ int main(int argc, char **argv)
         }
     }
 
-    build_trie();
-
+    trie_build();
+    scan_input();
     exit(EXIT_SUCCESS);
 }
